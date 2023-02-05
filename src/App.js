17077cae8bd2c0db15afe1a7e831b10fcaa5bf4f
Chanpreet Kaur <6kchanpreet@gmail.com>
2023-02-05 16:17:21 +0530

    created the body of react app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      newItem:"",
+      list:[]
+    }
+  }
+
+  
+  addItem(todoValue){
+    if(todoValue !==""){
+      const newItem={
+        id:Date.now(),
+        value:todoValue,
+        isDone:false
+      };
+      const list=[...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem:""
+      });
+    }
+  }
+
+  deleteItem(id){
+    const list=[...this.state.list];
+    const updatedList=list.filter(item=> item.id !==id);
+    this.setState({list:updatedList})
+  }
+
+  updateInput(input){
+    this.setState({newItem:input});
+  }
+
+  render(){
   return (
     <div>
       <img src={logo} alt="logo" width={100} height={100} className="logo" />
@@ -10,12 +47,32 @@ function App() {
       <div className='container'>
         Add an item
         <br />
-        <input type="text" className='input-text' placeholder='enter your todo' />
-        <button className='add-btn'> Add this todo</button>
+        <input type="text" 
+        className='input-text' 
+        placeholder='enter your todo' 
+        required value={this.state.newItem}
+        onChange={e=>this.updateInput(e.target.value)}/>
+        <button className='add-btn' 
+        onClick={()=>this.addItem(this.state.newItem)} 
+        disabled={!this.state.newItem.length} > Add this todo</button>
         <div className='list'>
           <ul>
+          {this.state.list.map(item=>{
+            return(
+              <li key={item.id}>
+                <input type="checkbox" 
+                name="idDone" 
+                checked={item.isDone} 
+                onChange={()=>{}} />
+                {item.value}
+                <button className='btn' onClick={() => this.deleteItem(item.id)}>Delete</button>
+              </li>
+            )
+          })}
             <li>
-              <input type="check" />
+              <input type="checkbox" name="" id='' />
+              React bootcamp
+              <button className='btn'>Delete</button>
             </li>
           </ul>
         </div>
@@ -23,6 +80,7 @@ function App() {
     </div>
     
   );
+  }
 }
 
 export default App;
